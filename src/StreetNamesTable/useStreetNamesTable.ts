86468cfbd44fd24ef283ac07f3ddf4003d5159a9
justingreco <justin.greco@gmail.com>
2025-07-09 18:40:58 -0400

    removed unused file

--- src/StreetNamesTable/useStreetNamesTable.ts
+++
@@ -1,8 +0,0 @@
-// Define any types for your hook's inputs and outputs
-interface UseStreetNamesTableOptions {}
-
-const useStreetNames = ({}: UseStreetNamesTableOptions) => {
-  return {};
-};
-
-export default useStreetNamesTable;
